86cc81ca5b2e3ddcd46f45b61745b1c6be7c617b
humplee <littleted1221@163.com>
2019-05-14 22:20:42 -0700

    practice 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,35 @@
 package practice09;
 
+import java.util.ArrayList;
+
 public class Klass {
+    int number;
+    Student leader;
+    //String name;
+    ArrayList<Student> savedStudentList;
+    Klass(int klass){
+        this.number=klass;
+        savedStudentList=new ArrayList<>();
+            }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+    public void assignLeader(Student leader){
+        if(savedStudentList.isEmpty()&&(!savedStudentList.contains(leader))){
+            System.out.print("It is not one of us.\n");
+            return;}
+        this.leader=leader;}
+
+    public Student getLeader() {
+
+        return leader;
+    }
+    public void appendMember(Student theStudent){
+        savedStudentList.add(theStudent);
+    }
 }
